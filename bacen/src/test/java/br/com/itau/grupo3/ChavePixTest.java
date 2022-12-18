1af64e570560a6612b4c1ec083e4b47ead3de9d2
Ingrid Anjos <ingrid_anjos@hotmail.com>
2022-12-18 13:24:50 -0300

    Test unitário chavePix no Bacen

--- bacen/src/test/java/br/com/itau/grupo3/ChavePixTest.java
+++
@@ -1,4 +0,0 @@
-package br.com.itau.grupo3;
-
-public class ChavePixTest {
-}
--- bacen/src/test/java/br/com/itau/grupo3/service/ChavePixServiceTest.java
+++ bacen/src/test/java/br/com/itau/grupo3/service/ChavePixServiceTest.java
@@ -1,16 +1,67 @@
 package br.com.itau.grupo3.service;
 
+
+import br.com.itau.grupo3.dto.request.ChavePixRequest;
+import br.com.itau.grupo3.dto.response.ChavePixResponse;
+import br.com.itau.grupo3.factory.Factory;
+import br.com.itau.grupo3.mapper.ChavePixMapper;
+import br.com.itau.grupo3.model.ChavePix;
+import br.com.itau.grupo3.repository.ChavePixRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.Optional;
 
+
+import static org.mockito.ArgumentMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
 class ChavePixServiceTest {
 
-    @Test
-    void salvar() {
+    @InjectMocks
+    ChavePixService chavePixService;
+
+    @Mock
+    ChavePixRepository chavePixRepository;
+    @Mock
+    ChavePixMapper chavePixMapper;
+
+    static ChavePixRequest chavePixRequest;
+    static ChavePix chavePix;
+    static ChavePixResponse chavePixResponse;
+
+    @BeforeEach
+    void setup() {
+        chavePixRequest = Factory.createChavePixRequest();
+        chavePixResponse = Factory.createChavePixResponse();
     }
 
     @Test
+    public void salvarTest() {
+
+        Mockito.when(chavePixMapper.requestToModel(any())).thenReturn(chavePix);
+        Mockito.when(chavePixRepository.save(any())).thenReturn(chavePix);
+
+        ChavePixResponse chavePixResponse1 = chavePixService.salvar(chavePixRequest);
+
+        Mockito.verify(chavePixMapper, Mockito.times(1)).modelToResponse(any());
+        Mockito.verify(chavePixRepository, Mockito.times(1)).save(any());
+    }
+    @Test
     void buscarPorChave() {
+        Mockito.when(chavePixRepository.findById(any())).thenReturn(Optional.of(chavePix));
+
+        //List<ChavePixResponse> chavePixResponse2 = chavePixService.buscarPorChave(chavePix);
+
+       // chavePixService.buscarPorChave(chavePixResponse);
+
+
+        Mockito.verify(chavePixRepository, Mockito.times(1)).findById(any());
     }
 }
